3cd18da565fc89abaa1fb7b7591acb0b2e8856ca
PiotrSzymkowiak <pss95@tlen.pl>
2019-05-22 16:07:39 +0200

    Grapple is now working

--- Assets/__Scripts/Grapple.cs
+++ Assets/__Scripts/Grapple.cs
@@ -11,12 +11,9 @@ public class Grapple : MonoBehaviour
   public float grappleLength = 7;
   public float grappleInLength = 0.5f;
   public int unsafeTileHealthPenalty = 2;
-  public TextAsset mapGrappleable;
 
   [Header("Defined dynamically")]
   public eMode mode = eMode.none;
-  public List<int> grappleTiles;
-  public List<int> unsafeTiles;
 
   private Dray dray;
   private Rigidbody2D rigid;
@@ -38,23 +35,6 @@ public class Grapple : MonoBehaviour
 
   void Awake()
   {
-    string gTiles = mapGrappleable.text;
-    gTiles = Utils.RemoveLineEndings(gTiles);
-    grappleTiles = new List<int>();
-    unsafeTiles = new List<int>();
-    for (int i = 0; i < gTiles.Length; i++)
-    {
-      switch(gTiles[i])
-      {
-        case 'S':
-          grappleTiles.Add(i);
-          break;
-        case 'X':
-          unsafeTiles.Add(i);
-          break;
-      }
-    }
-
     dray = GetComponent<Dray>();
     rigid = GetComponent<Rigidbody2D>();
     anim = GetComponent<Animator>();
@@ -103,73 +83,75 @@ public class Grapple : MonoBehaviour
 
   void FixedUpdate()
   {
-    //switch(mode)
-    //{
-    //  case eMode.gOut: //grapple goes out
-    //    p1 += directions[facing] * grappleSpd * Time.fixedDeltaTime;
-    //    grapHead.transform.position = p1;
-    //    grapLine.SetPosition(1, p1);
+    switch (mode)
+    {
+      case eMode.gOut: //grapple goes out
+        p1 += directions[facing] * grappleSpd * Time.fixedDeltaTime;
+        grapHead.transform.position = p1;
+        grapLine.SetPosition(1, p1);
 
-    //    //check if hit
-    //    int tileNum = TileCamera.GET_MAP(p1.x, p1.y);
-    //    if(grappleTiles.IndexOf(tileNum) != -1)
-    //    {
-    //      mode = eMode.gInHit;
-    //      break;
-    //    }
-    //    if((p1-p0).magnitude >= grappleLength)
-    //    {
-    //      mode = eMode.gInMiss;
-    //    }
-    //    break;
+        //check if hit
+        int layerMask = 1 << LayerMask.NameToLayer("Grappable");
+        RaycastHit2D hit = Physics2D.Raycast(p1, directions[facing], 0.1f, layerMask);     
+        if(hit)
+        {
+          mode = eMode.gInHit;
+          break;
+        }
 
-    //  case eMode.gInMiss: //miss, comes back with double speed
-    //    p1 -= directions[facing] * 2 * grappleSpd * Time.fixedDeltaTime;
-    //    if (Vector3.Dot((p1-p0), directions[facing]) > 0)
-    //    {
-    //      //grapple still before Dray
-    //      grapHead.transform.position = p1;
-    //      grapLine.SetPosition(1, p1);
-    //    }
-    //    else
-    //    {
-    //      StopGrapple();
-    //    }
-    //    break;
+        if ((p1 - p0).magnitude >= grappleLength)
+        {
+          mode = eMode.gInMiss;
+        }
+        break;
 
-    //  case eMode.gInHit: //hit, pulls Dray to the wall
-    //    float dist = grappleInLength + grappleSpd * Time.fixedDeltaTime;
-    //    if(dist > (p1-p0).magnitude)
-    //    {
-    //      p0 = p1 - (directions[facing] * grappleInLength);
-    //      transform.position = p0;
-    //      StopGrapple();
-    //      break;
-    //    }
-    //    p0 += directions[facing] * grappleSpd * Time.fixedDeltaTime;
-    //    transform.position = p0;
-    //    grapLine.SetPosition(0, p0);
-    //    grapHead.transform.position = p1;
-    //    break;
-    //}
+      case eMode.gInMiss: //miss, comes back with double speed
+        p1 -= directions[facing] * 2 * grappleSpd * Time.fixedDeltaTime;
+        if (Vector3.Dot((p1 - p0), directions[facing]) > 0)
+        {
+          //grapple still before Dray
+          grapHead.transform.position = p1;
+          grapLine.SetPosition(1, p1);
+        }
+        else
+        {
+          StopGrapple();
+        }
+        break;
+
+      case eMode.gInHit: //hit, pulls Dray to the wall
+        float dist = grappleInLength + grappleSpd * Time.fixedDeltaTime;        
+        if (dist > (p1 - p0).magnitude)
+        {
+          p0 = p1 - (directions[facing] * grappleInLength);
+          transform.position = p0;
+          StopGrapple();
+          break;
+        }
+        p0 += directions[facing] * grappleSpd * Time.fixedDeltaTime;
+        transform.position = p0;
+        grapLine.SetPosition(0, p0);
+        grapHead.transform.position = p1;
+        break;
+    }
   }
 
   void StopGrapple()
   {
-    //dray.enabled = true;
-    //drayColld.enabled = true;
+    dray.enabled = true;
+    drayColld.enabled = true;
 
-    ////check if tile is dangerous
-    //int tileNum = TileCamera.GET_MAP(p0.x, p0.y);
-    //if(mode == eMode.gInHit && unsafeTiles.IndexOf(tileNum) != -1)
+    //check if tile is dangerous
+    
+    //if (mode == eMode.gInHit && unsafeTiles.IndexOf(tileNum) != -1)
     //{
     //  //we're on a dangerous tile
     //  dray.ResetInRoom(unsafeTileHealthPenalty);
     //}
 
-    //grapHead.SetActive(false);
+    grapHead.SetActive(false);
 
-    //mode = eMode.none;
+    mode = eMode.none;
   }
 
   void OnTriggerEnter2D(Collider2D other)
