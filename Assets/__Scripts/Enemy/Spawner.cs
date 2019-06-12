4e7b6a778b6c18d5c147aead5bb93bce6f80ea71
PiotrSzymkowiak <pss95@tlen.pl>
2019-06-12 23:14:51 +0200

    spawners added and some fixes

---
+++ Assets/__Scripts/Enemy/Spawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+  public GameObject spawnPrefab;
+
+  float startSpawning = 0;
+
+  private void Start()
+  {
+    startSpawning = Time.time;
+  }
+
+  private void Update()
+  {
+    if(Time.time > startSpawning + 1f )
+    {
+      GameObject monster = Instantiate(spawnPrefab, transform.position, transform.rotation);
+      startSpawning = Time.time;
+    }
+  }
+}
